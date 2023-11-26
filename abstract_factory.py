1305feee54b742fd08a7362c1d9a423c3d5a4ea2
Kurogitsunee <90721264+Kurogitsunee@users.noreply.github.com>
2023-11-26 20:10:45 +0500

    Fix code

--- abstract_factory.py
+++ abstract_factory.py
@@ -1,84 +1,213 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 
-class AbstractCarFactory():
+class Icarbody(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def release_carbody(self):
+        pass
 
-    __meta_class__ = ABCMeta
+class Tuningcarbody(Icarbody):
+
+    def release_carbody(self):
+        print("Tuning car carbody created")
+
+
+class Exoticcarbody(Icarbody):
+
+    def release_carbody(self):
+        print("Exotic car carbody created")
+
+
+class Iengine(metaclass=ABCMeta):
 
     @abstractmethod
-    def carbody():
-        """Create carbody"""
+    def release_engine(self):
+        pass
+
+
+class Tuningengine(Iengine):
+
+    def release_engine(self):
+        print("Tuning car engine created")
+
+
+class Exoticengine(Iengine):
+
+    def release_engine(self):
+        print("Exotic car engine created")
+
+
+class Iwheels(metaclass=ABCMeta):
 
     @abstractmethod
-    def engine():
-        """Create engine"""
+    def release_wheels(self):
+        pass
+
+
+class Tuningwheels(Iwheels):
+
+    def release_wheels(self):
+        print("Tuning car wheels created")
+
+
+class Exoticwheels(Iwheels):
+
+    def release_wheels(self):
+        print("Exotic car wheels created")
+
+
+class Ibrakes(metaclass=ABCMeta):
 
     @abstractmethod
-    def wheels():
-        """Create wheels"""
+    def release_brakes(self):
+        pass
+
+
+class Tuningbrakes(Ibrakes):
+
+    def release_brakes(self):
+        print("Tuning car brakes created")
+
+
+class Exoticbrakes(Ibrakes):
+
+    def release_brakes(self):
+        print("Exotic car brakes created")
+
+
+class Isuspension(metaclass=ABCMeta):
 
     @abstractmethod
-    def brakes():
-        """Create brakes"""
+    def release_suspension(self):
+        pass
 
+
+class Tuningsuspension(Isuspension):
+
+    def release_suspension(self):
+        print("Tuning car suspension created")
+
+
+class Exoticsuspension(Isuspension):
+
+    def release_suspension(self):
+        print("Exotic car suspension created")
+
+
+class Icar(metaclass=ABCMeta):
+    
     @abstractmethod
-    def suspension():
-        """Create suspension"""
+    def release_car(self, carbody:Icarbody, engine:Iengine, wheels:Iwheels, brakes:Ibrakes, suspension:Isuspension):
+        pass
 
 
-class TuningCarFactory(AbstractCarFactory):
+class Tuningcar(Icar):
 
-    def __init__(self):
+    def release_car(self, carbody: Icarbody, engine: Iengine, wheels: Iwheels, brakes: Ibrakes, suspension: Isuspension):
         print("Tuning car created")
-
-    def carbody(self):
-        print("Created car body for Tuning car")
-
-    def engine(self):
-        print("Created engine for Tuning car")
-
-    def wheels(self):
-        print("Created wheels for Tuning car")
-
-    def brakes(self):
-        print("Created brakes for Tuning car")
-
-    def suspension(self):
-        print("Created suspension for Tuning car")
+        carbody.release_carbody()
+        engine.release_engine()
+        wheels.release_wheels()
+        brakes.release_brakes()
+        suspension.release_suspension()
 
 
-class ExoticCarFactory(AbstractCarFactory):
+class Exoticcar(Icar):
 
-    def __init__(self):
+    def release_car(self, carbody: Icarbody, engine: Iengine, wheels: Iwheels, brakes: Ibrakes, suspension: Isuspension):
         print("Exotic car created")
-
-    def carbody(self):
-        print("Created car body for Exotic car")
-
-    def engine(self):
-        print("Created engine for Exotic car")
-
-    def wheels(self):
-        print("Created wheels for Exotic car")
-
-    def brakes(self):
-        print("Created brakes for Exotic car")
-
-    def suspension(self):
-        print("Created suspension for Exotic car")
+        carbody.release_carbody()
+        engine.release_engine()
+        wheels.release_wheels()
+        brakes.release_brakes()
+        suspension.release_suspension()
 
 
-tuning_car = TuningCarFactory()
-tuning_car.carbody()
-tuning_car.engine()
-tuning_car.wheels()
-tuning_car.brakes()
-tuning_car.suspension()
+class Ifactory(metaclass=ABCMeta):
 
-print("-"*40)
+    @abstractmethod
+    def create_carbody(self) -> Icarbody:
+        pass
 
-exotic_car = ExoticCarFactory()
-exotic_car.carbody()
-exotic_car.engine()
-exotic_car.wheels()
-exotic_car.brakes()
-exotic_car.suspension()
+    @abstractmethod
+    def create_engine(self) -> Iengine:
+        pass
+
+    @abstractmethod
+    def create_wheels(self) -> Iwheels:
+        pass
+
+    @abstractmethod
+    def create_brakes(self) -> Ibrakes:
+        pass
+
+    @abstractmethod
+    def create_suspension(self) -> Isuspension:
+        pass
+
+    @abstractmethod
+    def create_car(self) -> Icar:
+        pass
+
+
+class Tuningfactory(Ifactory):
+
+    def create_carbody(self) -> Icarbody:
+        return Tuningcarbody()
+    
+    def create_engine(self) -> Iengine:
+        return Tuningengine()
+    
+    def create_wheels(self) -> Iwheels:
+        return Tuningwheels()
+    
+    def create_brakes(self) -> Ibrakes:
+        return Tuningbrakes()
+    
+    def create_suspension(self) -> Isuspension:
+        return Tuningsuspension()
+    
+    def create_car(self) -> Icar:
+        return Tuningcar()
+    
+
+class Exoticfactory(Ifactory):
+
+    def create_carbody(self) -> Icarbody:
+        return Exoticcarbody()
+    
+    def create_engine(self) -> Iengine:
+        return Exoticengine()
+    
+    def create_wheels(self) -> Iwheels:
+        return Exoticwheels()
+    
+    def create_brakes(self) -> Ibrakes:
+        return Exoticbrakes()
+    
+    def create_suspension(self) -> Isuspension:
+        return Exoticsuspension()
+    
+    def create_car(self) -> Icar:
+        return Exoticcar()
+    
+
+t_factory = Tuningfactory()
+t_carbody = t_factory.create_carbody()
+t_engine = t_factory.create_engine()
+t_wheels = t_factory.create_wheels()
+t_brakes = t_factory.create_brakes()
+t_suspension = t_factory.create_suspension()
+t_car = t_factory.create_car()
+
+t_car.release_car(t_carbody, t_engine, t_wheels, t_brakes, t_suspension)
+
+e_factory = Exoticfactory()
+e_carbody = e_factory.create_carbody()
+e_engine = e_factory.create_engine()
+e_wheels = e_factory.create_wheels()
+e_brakes = e_factory.create_brakes()
+e_suspension = e_factory.create_suspension()
+e_car = e_factory.create_car()
+
+e_car.release_car(e_carbody, e_engine, e_wheels, e_brakes, e_suspension)
